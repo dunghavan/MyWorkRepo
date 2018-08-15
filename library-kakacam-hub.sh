7fff81825e5e682c36c5a810286b31469c5ef5da
dunghavan <dunghavan47@gmail.com>
2018-08-15 17:24:13 +0700

    15-8-2018

---
+++ library-kakacam-hub.sh
@@ -0,0 +1,9 @@
+# kakacam:
+go get github.com/streadway/amqp
+go get github.com/mitchellh/mapstructure
+go get github.com/fatih/structs
+go get github.com/go-sql-driver/mysql
+go get github.com/dgrijalva/jwt-go
+go get github.com/deckarep/golang-set
+go get github.com/gomodule/redigo/redis
+
---
+++ library-kakacam.sh
@@ -0,0 +1,7 @@
+
+# kakacam-hub:
+sudo apt-get install libavutil-dev
+go get github.com/imkira/go-libav/avutil
+go get github.com/davecgh/go-spew/spew
+go get github.com/hashicorp/go-version
+sudo apt-get install libavcodec-dev
